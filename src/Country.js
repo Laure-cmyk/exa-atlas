e70c8135781147cbbabd386b71a676e6af7f5a05
Laure-cmyk <laure.mangold@heig-vd.ch>
2025-08-28 11:17:27 +0200

    First commit

---
+++ src/Country.js
@@ -0,0 +1,47 @@
+
+export default class Country {
+
+    constructor(name, flag) {
+        this.name = name;
+        this.flag = flag;
+        this.population = population;
+        /* this.#countryElement = this.#generateElement(); */
+    }
+
+    #generateElement() {
+        const countryContainer = document.createElement("div");
+        countryContainer.classList.add("country-container");
+
+        countryContainer.innerHTML = `
+        <article class="country--card">
+			<section class="country--flag">
+				<h1>${flag}</h1>
+			</section>
+
+			<section class="country--name">
+				<h1>${name}</h1>
+				<h2>${official}</h2>
+			</section>
+
+			<section class="country--info">
+				<p>population: ${population}/p>
+				<p>region: ${region}</p>
+			</section>
+
+			<section class="country--actions">
+				<a class="country--map" href="${position}" target="_blank">
+					Display Map 📍
+				</a>
+			</section>
+		</article>
+        `
+    }
+
+    render() {
+        const main = document.querySelector("main");
+        main.append(countryContainer);
+        return main;
+    }
+
+
+}
---
+++ src/app.js
@@ -0,0 +1,120 @@
+"use strict"
+
+import Country from "./Country.js"
+
+const main = document.querySelector("main");
+const searchInput = document.querySelector("input");
+const form = document.querySelector("form");
+
+
+function logSubmit(event) {
+    const value = searchInput.value;
+    console.log("submitted " + value);
+    event.preventDefault();
+
+
+    const BASE_URL = `https://restcountries.com/v3.1/name/${value}`;
+/* const BASE_URL = 'https://restcountries.com/v3.1/name/{name}'; */
+
+
+const getCountries = async () => {
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+
+
+/*     if (!response) {
+        const div = document.querySelector(".search--container");
+        const errorElement = document.createElement("p");
+        errorElement.classList.add("error");
+        errorElement.innerText = "Couldn't find country";
+        div.insertAdjacentElement('beforeend', errorElement);
+    }
+ */
+    const countryContainer = document.createElement("div");
+        countryContainer.classList.add("country-container");
+
+        countryContainer.innerHTML = `
+        <article class="country--card">
+			<section class="country--flag">
+				<h1>${data[0].flag}</h1>
+			</section>
+
+			<section class="country--name">
+				<h1>${data[0].name.common}</h1>
+				<h2>${data[0].name.official}</h2>
+			</section>
+
+			<section class="country--info">
+				<p>population: ${data[0].population}</p>
+				<p>region: ${data[0].region}</p>
+			</section>
+
+			<section class="country--actions">
+				<a class="country--map" href="${data[0].maps.openStreetMaps}" target="_blank">
+					Display Map 📍
+				</a>
+			</section>
+		</article>
+        `
+
+        main.append(countryContainer);
+}; 
+
+getCountries();
+}
+
+
+form.addEventListener("submit", logSubmit);
+
+
+
+
+
+/* May be reused later if I have the time to make this class work */
+// Test url
+/* const BASE_URL = `https://restcountries.com/v3.1/name/${searchInput.value}`;
+// API URL
+//const BASE_URL = 'https://restcountries.com/v3.1/name/{name}';
+
+
+const getCountries = async () => {
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+
+    console.log(data);
+    console.log(data[0].flag);
+    console.log(data[0].name.common);
+
+    const countryContainer = document.createElement("div");
+        countryContainer.classList.add("country-container");
+
+        countryContainer.innerHTML = `
+        <article class="country--card">
+			<section class="country--flag">
+				<h1>${data[0].flag}</h1>
+			</section>
+
+			<section class="country--name">
+				<h1>${data[0].name.common}</h1>
+				<h2>${data[0].name.official}</h2>
+			</section>
+
+			<section class="country--info">
+				<p>population: ${data[0].population}</p>
+				<p>region: ${data[0].region}</p>
+			</section>
+
+			<section class="country--actions">
+				<a class="country--map" href="${data[0].maps.openStreetMaps}" target="_blank">
+					Display Map 📍
+				</a>
+			</section>
+		</article>
+        `
+
+        main.append(countryContainer);
+}; 
+
+getCountries(); */
+
+
